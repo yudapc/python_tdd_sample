6f43ccfd12103f990b8b7326bb953142d20ad2b6
Yuda Cogati <yudapc@gmail.com>
2020-05-04 21:39:28 +0700

    added basicmath with methods div, plus, sum

---
+++ basicmath.py
@@ -0,0 +1,11 @@
+def div(a=0, b=0):
+    return a / b
+
+def plus(a=0, b=0):
+    return a + b
+
+def sum(dataArr=[]):
+    total = 0
+    for data in dataArr:
+        total += data
+    return total
